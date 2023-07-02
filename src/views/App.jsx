ac6ba8d95127dc75ea191deb9588d1f06c6f829a
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-07-02 10:51:45 +0700

    DONE : Extras Pages

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,8 +1,16 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-import LayoutDefault from "../layout/LayoutDefault";
-import { Component, Suspense } from "react";
+import React, { Component, Suspense } from "react";
 import LoadingApp from "../components/LoadingApp";
 import "../scss/style.scss";
+
+// Layout
+const LayoutDefault = React.lazy(() => import('../layout/LayoutDefault'))
+
+// Pages
+const Login = React.lazy(() => import('./pages/login/Login'))
+const Register = React.lazy(() => import('./pages/register/Register'))
+const Page404 = React.lazy(() => import('./pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./pages/page500/Page500'))
 
 class App extends Component {
   render() {
@@ -10,6 +18,10 @@ class App extends Component {
       <HashRouter>
         <Suspense fallback={<LoadingApp />}>
           <Routes>
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+            <Route exact path="/register" name="Register Page" element={<Register />} />
+            <Route exact path="/404" name="Page 404" element={<Page404 />} />
+            <Route exact path="/500" name="Page 500" element={<Page500 />} />
             <Route path="*" element={<LayoutDefault />} />
           </Routes>
         </Suspense>
@@ -17,8 +29,5 @@ class App extends Component {
     );
   }
 }
-
-// Layout
-// const LayoutDefault = React.lazy(() => import('../layout/LayoutDefault'))
 
 export default App;
