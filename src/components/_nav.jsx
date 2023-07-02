07af6286ae05ac07b72ef51ba1400ba43c1c61df
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-07-02 10:27:30 +0700

    DONE : Component Forms

--- src/components/_nav.jsx
+++ src/components/_nav.jsx
@@ -1,4 +1,4 @@
-import { cilCursor, cilDrop, cilPencil, cilPuzzle, cilSpeedometer } from "@coreui/icons";
+import { cilCursor, cilDrop, cilNotes, cilPencil, cilPuzzle, cilSpeedometer } from "@coreui/icons";
 import CIcon from "@coreui/icons-react";
 import { CNavGroup, CNavItem, CNavTitle } from "@coreui/react";
 import React from "react";
@@ -132,6 +132,53 @@ const _nav = [
         component: CNavItem,
         name: 'Dropdowns',
         to: '/buttons/dropdowns',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Forms',
+    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Form Control',
+        to: '/forms/form-control',
+      },
+      {
+        component: CNavItem,
+        name: 'Select',
+        to: '/forms/select',
+      },
+      {
+        component: CNavItem,
+        name: 'Checks & Radios',
+        to: '/forms/checks-radios',
+      },
+      {
+        component: CNavItem,
+        name: 'Range',
+        to: '/forms/range',
+      },
+      {
+        component: CNavItem,
+        name: 'Input Group',
+        to: '/forms/input-group',
+      },
+      {
+        component: CNavItem,
+        name: 'Floating Labels',
+        to: '/forms/floating-labels',
+      },
+      {
+        component: CNavItem,
+        name: 'Layout',
+        to: '/forms/layout',
+      },
+      {
+        component: CNavItem,
+        name: 'Validation',
+        to: '/forms/validation',
       },
     ],
   },
--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -28,6 +28,16 @@ const Buttons = React.lazy(() => import('../views/buttons/buttons/Buttons'))
 const ButtonGroups = React.lazy(() => import('../views/buttons/button-groups/ButtonGroups'))
 const Dropdowns = React.lazy(() => import('../views/buttons/dropdowns/Dropdowns'))
 
+//Forms
+const FormControl = React.lazy(() => import('../views/forms/form-control/FormControl'))
+const Select = React.lazy(() => import('../views/forms/select/Select'))
+const ChecksRadios = React.lazy(() => import('../views/forms/checks-radios/ChecksRadios'))
+const Range = React.lazy(() => import('../views/forms/range/Range'))
+const InputGroup = React.lazy(() => import('../views/forms/input-group/InputGroup'))
+const FloatingLabels = React.lazy(() => import('../views/forms/floating-labels/FloatingLabels'))
+const Layout = React.lazy(() => import('../views/forms/layout/Layout'))
+const Validation = React.lazy(() => import('../views/forms/validation/Validation'))
+
 const routes = [
   // Dashboard
   { path: "/", exact: true, name: "Home" },
@@ -60,6 +70,17 @@ const routes = [
   { path: '/buttons/buttons', name: 'Buttons', element: Buttons },
   { path: '/buttons/button-groups', name: 'Button Groups', element: ButtonGroups },
   { path: '/buttons/dropdowns', name: 'Dropdowns', element: Dropdowns },
+
+  // Forms
+  { path: '/forms', name: 'Forms', element: FormControl, exact: true },
+  { path: '/forms/form-control', name: 'Form Control', element: FormControl },
+  { path: '/forms/select', name: 'Select', element: Select },
+  { path: '/forms/checks-radios', name: 'Checks & Radios', element: ChecksRadios },
+  { path: '/forms/range', name: 'Range', element: Range },
+  { path: '/forms/input-group', name: 'Input Group', element: InputGroup },
+  { path: '/forms/floating-labels', name: 'Floating Labels', element: FloatingLabels },
+  { path: '/forms/layout', name: 'Layout', element: Layout },
+  { path: '/forms/validation', name: 'Validation', element: Validation },
 ];
 
 export default routes;
